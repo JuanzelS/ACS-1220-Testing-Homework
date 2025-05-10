e7dd2b52098c25fddaeddfd2a8682f95f5650d55
JuanzelS <zel.staton@gmail.com>
2025-05-10 00:11:05 -0700

    commit

--- books_app/auth/tests.py
+++ books_app/auth/tests.py
@@ -1,8 +1,6 @@
 import os
 from unittest import TestCase
-
 from datetime import date
- 
 from books_app.extensions import app, db, bcrypt
 from books_app.models import Book, Author, User, Audience
 
@@ -23,7 +21,6 @@ def create_books():
         author=a1
     )
     db.session.add(b1)
-
     a2 = Author(name='Sylvia Plath')
     b2 = Book(title='The Bell Jar', author=a2)
     db.session.add(b2)
@@ -41,7 +38,7 @@ def create_user():
 
 class AuthTests(TestCase):
     """Tests for authentication (login & signup)."""
- 
+    
     def setUp(self):
         """Executed prior to each test."""
         app.config['TESTING'] = True
@@ -51,47 +48,126 @@ class AuthTests(TestCase):
         self.app = app.test_client()
         db.drop_all()
         db.create_all()
-
+    
     def test_signup(self):
         # TODO: Write a test for the signup route. It should:
         # - Make a POST request to /signup, sending a username & password
         # - Check that the user now exists in the database
-        pass
-
+        post_data = {
+            'username': 'new_user',
+            'password': 'new_password'
+        }
+        response = self.app.post('/signup', data=post_data, follow_redirects=True)
+        
+        # Check that the user now exists in the database
+        self.assertEqual(response.status_code, 200)
+        user = User.query.filter_by(username='new_user').first()
+        self.assertIsNotNone(user)
+    
     def test_signup_existing_user(self):
         # TODO: Write a test for the signup route. It should:
         # - Create a user
         # - Make a POST request to /signup, sending the same username & password
         # - Check that the form is displayed again with an error message
-        pass
-
+        
+        # Create a user
+        create_user()
+        
+        # Make a POST request to /signup with the same username
+        post_data = {
+            'username': 'me1',
+            'password': 'password'
+        }
+        response = self.app.post('/signup', data=post_data, follow_redirects=True)
+        
+        # Check that the form is displayed again with an error message
+        self.assertEqual(response.status_code, 200)
+        response_text = response.get_data(as_text=True)
+        self.assertIn('That username is taken', response_text)
+    
     def test_login_correct_password(self):
         # TODO: Write a test for the login route. It should:
         # - Create a user
         # - Make a POST request to /login, sending the created username & password
         # - Check that the "login" button is not displayed on the homepage
-        pass
-
+        
+        # Create a user
+        create_user()
+        
+        # Make a POST request to /login
+        post_data = {
+            'username': 'me1',
+            'password': 'password'
+        }
+        response = self.app.post('/login', data=post_data, follow_redirects=True)
+        
+        # Check that the "login" button is not displayed on the homepage
+        self.assertEqual(response.status_code, 200)
+        response_text = response.get_data(as_text=True)
+        self.assertNotIn('Log In', response_text)
+    
     def test_login_nonexistent_user(self):
         # TODO: Write a test for the login route. It should:
         # - Make a POST request to /login, sending a username & password
         # - Check that the login form is displayed again, with an appropriate
-        #   error message
-        pass
-
+        # error message
+        
+        # Make a POST request to /login with a nonexistent user
+        post_data = {
+            'username': 'nonexistent_user',
+            'password': 'password'
+        }
+        response = self.app.post('/login', data=post_data, follow_redirects=True)
+        
+        # Check that the login form is displayed again with an error message
+        self.assertEqual(response.status_code, 200)
+        response_text = response.get_data(as_text=True)
+        self.assertIn('No user with that username', response_text)
+    
     def test_login_incorrect_password(self):
         # TODO: Write a test for the login route. It should:
         # - Create a user
         # - Make a POST request to /login, sending the created username &
-        #   an incorrect password
+        # an incorrect password
         # - Check that the login form is displayed again, with an appropriate
-        #   error message
-        pass
-
+        # error message
+        
+        # Create a user
+        create_user()
+        
+        # Make a POST request to /login with incorrect password
+        post_data = {
+            'username': 'me1',
+            'password': 'wrong_password'
+        }
+        response = self.app.post('/login', data=post_data, follow_redirects=True)
+        
+        # Check that the login form is displayed again with an error message
+        self.assertEqual(response.status_code, 200)
+        response_text = response.get_data(as_text=True)
+        self.assertIn('Password doesn\'t match', response_text)
+    
     def test_logout(self):
         # TODO: Write a test for the logout route. It should:
         # - Create a user
         # - Log the user in (make a POST request to /login)
         # - Make a GET request to /logout
         # - Check that the "login" button appears on the homepage
-        pass
+        
+        # Create a user
+        create_user()
+        
+        # Log the user in
+        post_data = {
+            'username': 'me1',
+            'password': 'password'
+        }
+        self.app.post('/login', data=post_data, follow_redirects=True)
+        
+        # Make a GET request to /logout
+        response = self.app.get('/logout', follow_redirects=True)
+        
+        # Check that the "login" button appears on the homepage
+        self.assertEqual(response.status_code, 200)
+        response_text = response.get_data(as_text=True)
+        self.assertIn('Log In', response_text)
